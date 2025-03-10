59b232317ac1db526a87ae16eaf32de245f2a63c
Kirill <151939358+KirillStetsenko@users.noreply.github.com>
2025-03-10 23:59:06 +0200

    test with token and ID

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 
 <template>
-  <h1 class="text-5xl">Hello</h1>
+  <h1 class="text-5xl">Hello. This is test file with token and ID</h1>
 </template>
 
 <style scoped></style>
