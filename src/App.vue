b818a40b6f22ec94e50c96a7d2058eb463c0f8b3
Kirill <151939358+KirillStetsenko@users.noreply.github.com>
2025-03-11 00:08:18 +0200

    control CI/CD autodeploy test

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <script setup></script>
 
 <template>
-  <h1 class="text-5xl">Hello. This is test file with token and ID</h1>
+  <div class="bg-gray-700 h-screen">
+    <h1 class="text-5xl text-blue-500">
+      Hello. This is test file with token and ID. Control test. All right
+    </h1>
+  </div>
 </template>
 
 <style scoped></style>
