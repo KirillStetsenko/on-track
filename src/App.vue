71ed7585286a5a929e26982bccef36ca3e2776c5
Kirill <151939358+KirillStetsenko@users.noreply.github.com>
2025-03-11 17:07:59 +0200

    deploy to github manually

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 
 <template>
   <div class="bg-gray-700 h-screen">
-    <h1 class="text-5xl text-blue-500">Deploy to GitHub automaticly</h1>
+    <h1 class="text-5xl text-blue-500">Deploy to GitHub pages manually</h1>
   </div>
 </template>
 
