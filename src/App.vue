ace3f91dd9d06f24a007052c6c9d15fd6ec3a56d
Kirill <151939358+KirillStetsenko@users.noreply.github.com>
2025-03-11 14:56:52 +0200

    deploy to netlify and vercel

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,6 @@
       Deploy to <span class="text-cyan-300">Netlify</span> and
       <span class="text-orange-400"> Vercel </span>!
     </h1>
-    <div class="text-green-400 text-5xl">FINAL TEST!!! IT WORKS</div>
   </div>
 </template>
 
