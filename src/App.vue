ac289a08de0f8ec49bc97cb777be0705b43ddca7
Kirill <151939358+KirillStetsenko@users.noreply.github.com>
2025-03-11 17:45:38 +0200

    deploy with Actions

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,7 @@
 
 <template>
   <div class="bg-gray-700 h-screen">
-    <h1 class="text-5xl text-blue-500">
-      Deploy to GitHub with deploy from branch
-    </h1>
+    <h1 class="text-5xl text-blue-500">Deploy to GitHub with Actions</h1>
   </div>
 </template>
 
