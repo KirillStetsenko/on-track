1fd5b8a7d259ff264cce5ce5cc1a04c4a1e0af42
Kirill <151939358+KirillStetsenko@users.noreply.github.com>
2025-03-11 03:24:02 +0200

    deploy to netlify and vercel

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,7 @@
   <div class="bg-gray-700 h-screen">
     <h1 class="text-5xl text-blue-500">
       Deploy to <span class="text-cyan-300">Netlify</span> and
-      <span class="text-orange-400"> Vercel </span> with org_id project_is and
-      vercel_id
+      <span class="text-orange-400"> Vercel </span>!
     </h1>
   </div>
 </template>
